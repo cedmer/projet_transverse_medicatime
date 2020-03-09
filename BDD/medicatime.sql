32ea33185a958e984e4178f122bc3656b4f7b237
Mathieu Cantagrel <mathieu.cantagrel@efrei.net>
2020-03-09 10:20:26 +0100

    update database

--- BDD/medicatime.sql
+++ BDD/medicatime.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 02 mars 2020 à 17:38
+-- Généré le :  lun. 09 mars 2020 à 10:19
 -- Version du serveur :  10.4.6-MariaDB
 -- Version de PHP :  7.3.9
 
@@ -49,34 +49,6 @@ CREATE TABLE `tableau` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `tableau`
---
-
-INSERT INTO `tableau` (`tid`, `medicament`, `jour`, `heure`, `dosage`) VALUES
-(78, 'test4', '1', '14', '45'),
-(77, 'test3', '4', '12', '12'),
-(76, 'test3', '4', '8', '12'),
-(75, 'test2', '1', '20', '12'),
-(74, 'test2', '1', '16', '12'),
-(73, 'test2', '1', '10', '12'),
-(72, 'test', '6', '22', '12'),
-(71, 'test', '4', '22', '12'),
-(70, 'test', '3', '22', '12'),
-(69, 'test', '1', '22', '12'),
-(68, 'test', '6', '18', '12'),
-(67, 'test', '4', '18', '12'),
-(66, 'test', '3', '18', '12'),
-(65, 'test', '1', '18', '12'),
-(64, 'test', '6', '14', '12'),
-(63, 'test', '4', '14', '12'),
-(62, 'test', '3', '14', '12'),
-(61, 'test', '1', '14', '12'),
-(60, 'test', '6', '10', '12'),
-(59, 'test', '4', '10', '12'),
-(58, 'test', '3', '10', '12'),
-(57, 'test', '1', '10', '12');
-
---
 -- Index pour les tables déchargées
 --
 
@@ -106,7 +78,7 @@ ALTER TABLE `medicament`
 -- AUTO_INCREMENT pour la table `tableau`
 --
 ALTER TABLE `tableau`
-  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
