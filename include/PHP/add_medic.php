faf0b162abfbef7e312bafd9307fc590604b49a2
Mathieu Cantagrel <mathieu.cantagrel@efrei.net>
2020-03-13 15:22:38 +0100

    protection contre injection de code

--- include/PHP/add_medic.php
+++ include/PHP/add_medic.php
@@ -7,33 +7,37 @@ if($_POST['add_preview']){
     $medic = $_POST['medic'];
     $medic_mg = $_POST['medic_mg'];
 
+    if(!preg_match("/\s/", $medic)){
+        for ($i=8; $i<=22; $i+=2){
 
-    for ($i=8; $i<=22; $i+=2){
-
-        if ($i===22){
-            $heures = "nuit_check";
-        }else{
-            $heures = strval($i)."_check";
-        }
-        if($_POST[$heures])
-        {
-            for ($j=1; $j<=7; $j++)
+            if ($i===22){
+                $heures = "nuit_check";
+            }else{
+                $heures = strval($i)."_check";
+            }
+            if($_POST[$heures])
             {
-                $jours = strval($j)."_jour";
-
-                if ($_POST[$jours])
+                for ($j=1; $j<=7; $j++)
                 {
-                    $sql = "INSERT INTO tableau(medicament,jour,heure,dosage)
+                    $jours = strval($j)."_jour";
+
+                    if ($_POST[$jours])
+                    {
+                        $sql = "INSERT INTO tableau(medicament,jour,heure,dosage)
                         VALUES
                         ('$medic','$j','$i', '$medic_mg')";
 
-                    if(!mysqli_query($conn, $sql)){
-                        die("Error: ".mysqli_error($conn));
+                        if(!mysqli_query($conn, $sql)){
+                            die("Error: ".mysqli_error($conn));
+                        }
                     }
                 }
             }
         }
     }
+
+
+
 }
 
 mysqli_close($conn);
