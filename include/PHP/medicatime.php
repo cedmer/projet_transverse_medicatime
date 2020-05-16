9bf61d95e1ebe7b9303304e15d2036abf985d881
Cédric <ced.mer@laposte.net>
2020-05-16 16:51:07 +0200

    correction bouton chager

--- include/PHP/medicatime.php
+++ include/PHP/medicatime.php
@@ -33,7 +33,7 @@ include "tab_preview.php";
         </div>
     </div>
 
-   <div id="main">
+    <div id="main">
         <div id="forms">
 
             <form method="post" action="add_medic.php">
@@ -104,17 +104,18 @@ include "tab_preview.php";
                     <tr>
                         <td><input class="hvr-grow" type="submit" value="Prévisualiser" id="add_preview" name="add_preview"></td>
                         <td></td>
-                        </form>
+            </form>
 
-                        <td><form method="post" action="new_table.php" id="new">
-                                <input class="hvr-grow" type="submit" name="new_table" value="Vider le tableau" id="new_table_btn">
-                            </form>
-                        <td></td>
-                    </tr>
-                </table>
-            
+            <td>
+                <form method="post" action="new_table.php" id="new">
+                    <input class="hvr-grow" type="submit" name="new_table" value="Vider le tableau" id="new_table_btn">
+                </form>
+            <td></td>
+            </tr>
+            </table>
+
         </div>
-    </div>    
+    </div>
 
     <table id="table_medic">
         <?php
@@ -131,83 +132,14 @@ include "tab_preview.php";
     <script src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="../../JS/autocomplete.js"></script>
 
-
-
-
-
-    <?php
-
-        if (isset($_POST['Enregistrer']) and isset($_POST['mail']))
-        {
-            $mail=$_POST['mail'];
-
-            if($mail!=NULL)
-            {
-                $is_username_unique=$bdd->prepare('SELECT * FROM patient WHERE mail = ?');      //on vérifie que le patient existe via le mail qui est unique
-                $is_username_unique->execute(array($mail));
-                $is_username_unique->closeCursor();
-                $count=$is_username_unique->rowCount();
-
-                if($count!=0)
-                {
-                    $reqpid = $bdd->prepare("SELECT pid FROM patient WHERE mail = :mail");      //on récupère son pid
-                    $reqpid->execute(array('mail' => $mail));
-                    $data=$reqpid->fetch(PDO::FETCH_OBJ);     
-                    $pid = $data->pid; 
-
-                    $req=$bdd->prepare('DELETE from tableau_patient WHERE pid = :pid');    //on supprime
-                    $req->execute(array(
-                    'pid' => $pid
-                    ));
-                    $req->closeCursor();
-
-                    $reqtab = $bdd->prepare("SELECT medicament, jour, heure, dosage FROM tableau");     //on récupère les données du tableau
-                    $reqtab->execute(array());
-                    $data=$reqtab->fetchAll();                              //data est un tableau 2D contenant les données médicament, jour, heure, dosage
-
-                    foreach ($data as list($a, $b, $c, $d)) {               //a chaque itération, $a, $b, $c, $d prennent respectivement medicament, jour, heure, dosage (une boucle = une ligne)
-                        $medicament = $a;
-                        $jour = $b;
-                        $heure = $c;
-                        $dosage = $d;
-
-                    $req=$bdd->prepare('INSERT INTO tableau_patient(medicament,jour,heure,dosage,pid) VALUES(:medicament,:jour,:heure,:dosage,:pid)');    //on ajoute dans tableau_patient avec son pid
-                    $req->execute(array(
-                        'medicament'=>$medicament,
-                        'jour'=>$jour,
-                        'heure'=>$heure,
-                        'dosage'=>$dosage,
-                        'pid'=>$pid
-                    ));
-                    $req->closeCursor();
-                    }
-
-                    echo'<p id="Error">Le calendrier a été ajouté à ce patient.</p>';  
-                }
-                else
-                {
-                    echo'<p id="Error">Informations invalides. Ce mail est introuvable.</p>';  
-                }
-            }
-            else
-            {
-                echo'<p id="Error">Veuillez remplir le mail.</p>';  
-            } 
-        }
-
-    ?>
-
-
-
-
     <div class="formbox">
-
-        <form method="POST" id="form">
+        <form method="POST" id="form" action="Charger_enregistrer.php">
             <label>Vous avez terminé ? Remplissez le mail du patient et enregistrez.</label><br>
             <input size="30" type="mail" name="mail" class="logbox" placeholder="Mail"><br><br>
             <input class="hvr-grow" type="submit" name="Enregistrer" id="submitbox" value="Enregistrer">
+            <input class="hvr-grow" type="submit" name="Charger" id="submitbox" value="Charger">
         </form>
-        
+
     </div>
 
 </body>
