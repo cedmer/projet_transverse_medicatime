30e270767a8e46d568bbbde0b35d1e4f0e2cf1b5
Cédric <ced.mer@laposte.net>
2020-05-16 21:28:36 +0200

    css Mainpage temp

--- include/PHP/Mainpage.php
+++ include/PHP/Mainpage.php
@@ -35,129 +35,137 @@ include "tab_preview.php";
                         ?>
                     </table>
                 </div>
+                <div class="container_pdf">
+                    <div class=" col-md-10" class="button_pdf_1">
+                        <form action="pdf_generator.php" method="post">
+                            <input type="submit" value="pdf" id="pdf_2">
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="row">
-        <div class=" col-md-10" id="container_form">
-            <div class="col-md-10" id="form">
-                <form method="post" action="new_table.php" id="new">
-                    <input type="submit" name="new_table" value="Vider le tableau" id="new_table_btn">
-                </form>
-                <form method="post" action="add_medic.php">
 
-                    <table>
-                        <tr>
-                            <td><label>medicament</label></td>
-                            <td><input type="text" placeholder="medicament" required="required" name="medic" id="medic" pattern="[^0-9]*"></td>
-                            <td><label>mg</label></td>
-                            <td><input type="number" min="0" placeholder="mg" required="required" name="medic_mg" id="medic_mg"></td>
-                        </tr>
-                        <tr id="block">
-                            <td><label> </label></td>
-                            <td>
-                                <div id="response"></div>
-                            </td>
+        <div class="row">
+            <div class=" col-md-10" id="container_form">
+                <div class="col-md-6" id="form">
+                    <form method="post" action="add_medic.php">
 
-                        </tr>
-                        <tr>
+                        <table id="champs">
+                            <tr>
+                                <td><label>medicament</label></td>
+                                <td><input type="text" placeholder="medicament" required="required" name="medic" id="medic" pattern="[^0-9]*"></td>
+                                <td><label>mg</label></td>
+                                <td><input type="number" min="0" placeholder="mg" required="required" name="medic_mg" id="medic_mg"></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>
+                                    <div id="response"></div>
+                                </td>
+                                <td></td>
+                                <td></td>
 
-                        </tr>
-                        <tr>
-                            <td><label>lundi</label></td>
-                            <td><input type="checkbox" name="1_jour"></td>
-                            <td><label>8h</label></td>
-                            <td><input type="checkbox" name="8_check"></td>
-                        </tr>
-                        <tr>
-                            <td><label>mardi</label></td>
-                            <td><input type="checkbox" name="2_jour"></td>
-                            <td><label>10h</label></td>
-                            <td><input type="checkbox" name="10_check"></td>
-                        </tr>
-                        <tr>
-                            <td><label>mercredi</label></td>
-                            <td><input type="checkbox" name="3_jour"></td>
-                            <td><label>12h</label></td>
-                            <td><input type="checkbox" name="12_check"></td>
-                        </tr>
-                        <tr>
-                            <td><label>jeudi</label></td>
-                            <td><input type="checkbox" name="4_jour"></td>
-                            <td><label>14h</label></td>
-                            <td><input type="checkbox" name="14_check"></td>
-                        </tr>
-                        <tr>
-                            <td><label>vendredi</label></td>
-                            <td><input type="checkbox" name="5_jour"></td>
-                            <td><label>16h</label></td>
-                            <td><input type="checkbox" name="16_check"></td>
-                        </tr>
-                        <tr>
-                            <td><label>samedi</label></td>
-                            <td><input type="checkbox" name="6_jour"></td>
-                            <td><label>18h</label></td>
-                            <td><input type="checkbox" name="18_check"></td>
-                        </tr>
-                        <tr>
-                            <td><label>dimanche</label></td>
-                            <td><input type="checkbox" name="7_jour"></td>
-                            <td><label>20h</label></td>
-                            <td><input type="checkbox" name="20_check"></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td><label>nuit</label></td>
-                            <td><input type="checkbox" name="nuit_check"></td>
-                        </tr>
-                        <tr>
-                            <td><input type="button" name="every_day" value="Tous les jours" class="red_btn"></td>
-                            <td></td>
-                            <td><input type="button" name="every_hour" value="Toutes les heures" class="red_btn"></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td><input type="submit" value="Prévisualiser" id="add_preview" name="add_preview"></td>
-                            <td></td>
+                            </tr>
+                        </table>
+                        <table id="champs2">
+                            <tr>
+                                <td><label>lundi</label></td>
+                                <td><input type="checkbox" name="1_jour"></td>
+                                <td><label>8h</label></td>
+                                <td><input type="checkbox" name="8_check"></td>
+                            </tr>
+                            <tr>
+                                <td><label>mardi</label></td>
+                                <td><input type="checkbox" name="2_jour"></td>
+                                <td><label>10h</label></td>
+                                <td><input type="checkbox" name="10_check"></td>
+                            </tr>
+                            <tr>
+                                <td><label>mercredi</label></td>
+                                <td><input type="checkbox" name="3_jour"></td>
+                                <td><label>12h</label></td>
+                                <td><input type="checkbox" name="12_check"></td>
+                            </tr>
+                            <tr>
+                                <td><label>jeudi</label></td>
+                                <td><input type="checkbox" name="4_jour"></td>
+                                <td><label>14h</label></td>
+                                <td><input type="checkbox" name="14_check"></td>
+                            </tr>
+                            <tr>
+                                <td><label>vendredi</label></td>
+                                <td><input type="checkbox" name="5_jour"></td>
+                                <td><label>16h</label></td>
+                                <td><input type="checkbox" name="16_check"></td>
+                            </tr>
+                            <tr>
+                                <td><label>samedi</label></td>
+                                <td><input type="checkbox" name="6_jour"></td>
+                                <td><label>18h</label></td>
+                                <td><input type="checkbox" name="18_check"></td>
+                            </tr>
+                            <tr>
+                                <td><label>dimanche</label></td>
+                                <td><input type="checkbox" name="7_jour"></td>
+                                <td><label>20h</label></td>
+                                <td><input type="checkbox" name="20_check"></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td><label>nuit</label></td>
+                                <td><input type="checkbox" name="nuit_check"></td>
+                            </tr>
+                            <tr>
+                                <td id="transparent"></td>
+                                <td id="transparent"><input type="button" name="every_day" value="Tous les jours" class="red_btn"></td>
+                                <td id="transparent"></td>
+                                <td id="transparent"><input type="button" name="every_hour" value="Toutes les heures" class="red_btn"></td>
+                            </tr>
+                            <tr>
+                                <td id="transparent"></td>
+                                <td id="transparent"><input type="submit" value="Prévisualiser" id="add_preview" name="add_preview"></td>
 
-                            <td></td>
-                        </tr>
+                    </form>
+                    <td id="transparent"></td>
+                    <td id="transparent">
+                        <form method="post" action="new_table.php" id="new">
+                            <input type="submit" name="new_table" value="Vider le tableau" id="new_table_btn">
+                        </form>
+                    </td>
+                    </tr>
                     </table>
-                </form>
+                </div>
             </div>
         </div>
-    </div>
 
-    <table id="table_medic">
-        <?php
-        medic_preview();
-        ?>
-    </table>
-    <div class="button_pdf_2">
-        <form action="pdf_generator.php" method="post">
-            <input type="submit" value="pdf" id="pdf_2">
-        </form>
-    </div>
-    <script src="../../JS/removerequired.js"></script>
-    <script src="../../JS/all_days_hours.js"></script>
-    <script src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <script src="../../JS/autocomplete.js"></script>
+        <table id="table_medic">
+            <?php
+            medic_preview();
+            ?>
+        </table>
+        <div class="button_pdf_2">
+            <form action="pdf_generator.php" method="post">
+                <input type="submit" value="pdf" id="pdf_2">
+            </form>
+        </div>
+        <script src="../../JS/removerequired.js"></script>
+        <script src="../../JS/all_days_hours.js"></script>
+        <script src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script src="../../JS/autocomplete.js"></script>
 
 
-    <div class="formbox">
+        <div class="formbox">
 
-        <form method="POST" id="form" action="Charger_enregistrer.php   ">
-            <label>Mail du patient</label><br>
-            <input type="mail" name="mail" class="logbox" placeholder="Mail"><br><br>
-            <input type="submit" name="Enregistrer" id="submitbox" value="Enregistrer">
-            <input type="submit" name="Charger" id="submitbox" value="Charger">
-        </form>
+            <form method="POST" id="form" action="Charger_enregistrer.php   ">
+                <label>Mail du patient</label><br>
+                <input type="mail" name="mail" class="logbox" placeholder="Mail"><br><br>
+                <input type="submit" name="Enregistrer" id="submitbox" value="Enregistrer">
+                <input type="submit" name="Charger" id="submitbox" value="Charger">
+            </form>
 
-    </div>
-    </div>
-    </div>
+        </div>
     </div>
 
 </body>
--- include/PHP/add_medic.php
+++ include/PHP/add_medic.php
@@ -2,46 +2,39 @@
 include("library/conn.php");
 ini_set('display_errors', 'off');
 
-if($_POST['add_preview']){
+if ($_POST['add_preview']) {
     $heures = " ";
     $jours = " ";
     $medic = $_POST['medic'];
     $medic_mg = $_POST['medic_mg'];
 
-    if(!preg_match("/\s/", $medic)){
-        for ($i=8; $i<=22; $i+=2){
+    if (!preg_match("/\s/", $medic)) {
+        for ($i = 8; $i <= 22; $i += 2) {
 
-            if ($i===22){
+            if ($i === 22) {
                 $heures = "nuit_check";
-            }else{
-                $heures = strval($i)."_check";
+            } else {
+                $heures = strval($i) . "_check";
             }
-            if($_POST[$heures])
-            {
-                for ($j=1; $j<=7; $j++)
-                {
-                    $jours = strval($j)."_jour";
+            if ($_POST[$heures]) {
+                for ($j = 1; $j <= 7; $j++) {
+                    $jours = strval($j) . "_jour";
 
-                    if ($_POST[$jours])
-                    {
+                    if ($_POST[$jours]) {
                         $sql = "INSERT INTO tableau(medicament,jour,heure,dosage)
                         VALUES
                         ('$medic','$j','$i', '$medic_mg')";
 
-                        if(!mysqli_query($conn, $sql)){
-                            die("Error: ".mysqli_error($conn));
+                        if (!mysqli_query($conn, $sql)) {
+                            die("Error: " . mysqli_error($conn));
                         }
                     }
                 }
             }
         }
     }
-
 }
 
 mysqli_close($conn);
 
 die("<script>window.location.href= 'medicatime.php'</script>");
-
-
-
